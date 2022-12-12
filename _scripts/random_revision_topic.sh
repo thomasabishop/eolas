d964353222fd42a26d5030efab48bf4891d78abf
thomasabishop <tactonbishop@gmail.com>
2022-12-12 19:30:06 +0000

    Autosave: 2022-12-12 19:30:06

--- _scripts/random_revision_topic.sh
+++ _scripts/random_revision_topic.sh
@@ -3,8 +3,15 @@
 DIRS_TO_PARSE="../Algorithms ../Computer_Architecture ../Databases"
 
 for ele in $DIRS_TO_PARSE; do
-    find $ele -name "*.md" -type f >> list_of_files.list
+    MATCHES+=( $(find $ele -name "*.md" -type f) )
 done
 
+for i in ${MATCHES[@]}
+do
+    echo $i
+done
 
-#find ../ -name "*.md" -type f > list_of_files.list
+# Next steps:
+#     - Generate random number
+#     - Specify the range of the random number from 0 to length of $MATCHES
+#     - Return that index
