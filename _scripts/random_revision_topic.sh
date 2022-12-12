602213608063b60c7896798a2b3f3a4e1c935285
thomasabishop <tactonbishop@gmail.com>
2022-12-12 19:00:06 +0000

    Autosave: 2022-12-12 19:00:06

--- _scripts/random_revision_topic.sh
+++ _scripts/random_revision_topic.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-find / -type f > list_of_files.list
+DIRS_TO_PARSE="../Algorithms ../Computer_Architecture ../Databases"
+
+for ele in $DIRS_TO_PARSE; do
+    find $ele -name "*.md" -type f >> list_of_files.list
+done
+
+
+#find ../ -name "*.md" -type f > list_of_files.list
