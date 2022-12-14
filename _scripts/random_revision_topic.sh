b76f77f1ddc312732d1da9ac6ce8b3891eed2fb1
thomasabishop <tactonbishop@gmail.com>
2022-12-14 19:30:05 +0000

    Autosave: 2022-12-14 19:30:05

--- _scripts/random_revision_topic.sh
+++ _scripts/random_revision_topic.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# This script returns a random topic for me to revise
+
+# Choose source directories...
 DIRS_TO_PARSE="../Algorithms ../Computer_Architecture ../Databases"
 
+# Return array of all files belonging to source dirs...
 for ele in $DIRS_TO_PARSE; do
     FILE_MATCHES+=( $(find $ele -name "*.md" -type f) )
 done
 
+# Generate a random integer between 0 and the match array length...
 RANDOM_FILE_INDEX=$(( $RANDOM % ${#FILE_MATCHES[@]} + 0 ))
 
+# Return file matching that index...
 echo "Revise this topic: ${FILE_MATCHES[$RANDOM_FILE_INDEX]}"
