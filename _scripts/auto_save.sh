21282d7a8f9eeefa2f4eca92e486d95b866ff510
thomasabishop <tactonbishop@gmail.com>
2023-01-25 06:54:12 +0000

    Autosave: 2023-01-25 06:54:12

--- _scripts/auto_save.sh
+++ _scripts/auto_save.sh
@@ -4,7 +4,7 @@
 
 # It is aliased to `cs-update` in .zshrc
 
-NOTES_PATH="/Users/thomasbishop/prepos/computer_science"
+NOTES_PATH="/Users/thomasbishop/repos/computer_science"
 SPACE_TO_UNDERSCORE="/home/thomas/repos/computer_science/_scripts/space_to_underscore_filename.sh"
 CLEAN_IMAGE_DIRECTORY="/home/thomas/repos/computer_science/_scripts/clean_image_directory.sh"
 
