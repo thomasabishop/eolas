34b0b532f29cb3adaadebdfdb012e1ff8e3526ad
thomasabishop <tactonbishop@gmail.com>
2023-04-17 07:03:50 +0100

    update clean image dir script to use eolas

--- _scripts/clean_image_directory.sh
+++ _scripts/clean_image_directory.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # If there are images in _img/ that are not being used in the workspace, delete them
-find /home/thomas/repos/computer_science/_img -type f | while read filename; do
+find /home/thomas/repos/eolas/_img -type f | while read filename; do
     rg "${filename##*/}" ../ --type markdown >/dev/null 2>&1
     if [ "$?" -eq 1 ]; then
         echo "Deleted unused image: ${filename##*/}"
