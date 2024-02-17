48f0a386638950a8ed3e41105f8735d38655c700
thomasabishop <tactonbishop@gmail.com>
2024-02-17 12:17:25 +0000

    chore: add hypen to underscrore script for filenames

---
+++ scripts/hyphen_to_underscore.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Convert hypens in file names to underscores
+
+main() {
+    find . -depth -name '*-*' | while read fname; do
+        new_fname=$(echo $fname | tr "-" "_")
+        if [ -e $new_fname ]; then
+            echo "File $new_fname already exists. Not replacing $fname"
+        else
+            echo "Creating new file $new_fname to replace $fname"
+            mv "$fname" $new_fname
+        fi
+    done
+}
+
+# Run and pipe errors and feedback to logfile
+
+&>/dev/null
+main
