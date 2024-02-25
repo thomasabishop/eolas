c6e562e9d7b6e1a9f74be22dabf62abeef82a9d2
thomasabishop <tactonbishop@gmail.com>
2024-02-25 19:57:22 +0000

    Autosave: 2024-02-25 19:57:22

--- scripts/purge_images.sh
+++ scripts/purge_images.sh
@@ -2,7 +2,8 @@
 
 # If there are images in img/ that are not being used by the Zettelkasten, delete them
 find /home/thomas/repos/eolas/img -type f | while read filename; do
-    rg "${filename##*/}" ../ --type markdown >/dev/null 2>&1
+		pwd    
+		rg "${filename##*/}" ../ --type markdown >/dev/null 2>&1
     if [ "$?" -eq 1 ]; then
         echo "Deleted unused image: ${filename##*/}"
         # rm $filename
