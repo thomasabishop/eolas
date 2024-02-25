997d693f15cf1275c8c4fa0e7e77cceee7a84a21
thomasabishop <tactonbishop@gmail.com>
2024-02-25 19:55:06 +0000

    Autosave: 2024-02-25 19:55:06

--- scripts/purge_images.sh
+++ scripts/purge_images.sh
@@ -5,6 +5,6 @@ find /home/thomas/repos/eolas/img -type f | while read filename; do
     rg "${filename##*/}" ../ --type markdown >/dev/null 2>&1
     if [ "$?" -eq 1 ]; then
         echo "Deleted unused image: ${filename##*/}"
-        rm $filename
+        # rm $filename
     fi
 done
