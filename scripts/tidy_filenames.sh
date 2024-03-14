3346a0d630184dcf75489df97862df9f6a7ace7f
thomasabishop <tactonbishop@gmail.com>
2024-03-14 15:00:03 +0000

    Autosave: 2024-03-14 15:00:03

--- scripts/tidy_filenames.sh
+++ scripts/tidy_filenames.sh
@@ -4,12 +4,12 @@
 
 main() {
     find . -depth -name '*-*' -name '*.md' | while read fname; do
-        new_fname=$(echo $fname | tr " " "_" | tr "-" "_")
-        if [ -e $new_fname ]; then
+        new_fname=$(echo "$fname" | tr " _"  "_")
+        if [ -e "$new_fname" ]; then
             echo "File $new_fname already exists. Not replacing $fname"
         else
             echo "Creating new file $new_fname to replace $fname"
-            mv "$fname" $new_fname
+            mv "$fname" "$new_fname"
         fi
     done
 }
